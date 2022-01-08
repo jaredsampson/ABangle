b82e144bb61cb6560a3a4e417e1d57b815390b02
William Steele <johnsteele2009@live.co.uk>
2022-01-08 20:05:59 +0000

    Added helper functions for pdb renumbering

--- abangle/number.py
+++ abangle/number.py
@@ -7,6 +7,7 @@ from fastcore.xtras import dict2obj
 
 from Bio.PDB.PDBParser import PDBParser
 from Bio.PDB.PDBIO import PDBIO, Select
+from Bio.PDB.Entity import Entity
 from Bio.PDB.Structure import Structure
 from Bio.PDB.Chain import Chain
 from Bio.SeqRecord import SeqRecord
@@ -18,6 +19,9 @@ def get_structure(path: pathlib.Path) -> Structure:
     Args:
         data_path (pathlib.Path): Path to data
     """
+    if not isinstance(path, pathlib.Path):
+        path = pathlib.Path(path)
+
     parser = PDBParser(PERMISSIVE=True, QUIET=True)
     return parser.get_structure(path.stem, path)
 
@@ -48,6 +52,12 @@ def parse_chain_type(details: Dict[str, Any]) -> str:
         return 'H'
     else:
         raise ValueError(f'chain type {chain_type}')
+
+def get_gap_position_ids(numbering: Dict[str, Dict[str, Any]]) -> List:
+    return [res_id[:3] for res_id in list(numbering['L'].numbering) if res_id[-1] == '-']
+
+def detach_children(entity: Entity, children: Sequence):
+    for child in children: entity.detach_child(child)
     
 def number_sequences(sequences: Dict[str, SeqRecord], scheme: str = 'chothia') -> Dict[str, Dict[str, Any]]:
     """Takes a list of chain: sequence mappings and aligns the sequences to get their chain identifer and numbering
@@ -58,7 +68,7 @@ def number_sequences(sequences: Dict[str, SeqRecord], scheme: str = 'chothia') -
         scheme (str, optional): Numbering scheme to use. Can be one of: imgt, chothia, kabat or martin. Defaults to 'chothia'. 
 
     Returns:
-        Dict[str, Dict[str, Any]]: e.g. {'A': {'Chain': 'H', 'span': slice(0, 107, None), numbering: (' ', 1', ' ')}}
+        Dict[str, Dict[str, Any]]: e.g. {'A': {'Chain': 'H', 'span': slice(0, 107, None), numbering: (' ', 1', ' ', 'T)}}
     """
     input = [
         (chain, str(seq_record.seq)) 
@@ -69,7 +79,7 @@ def number_sequences(sequences: Dict[str, SeqRecord], scheme: str = 'chothia') -
     
     numbering = [
         [(' ', res_id[0][0], res_id[0][1]) for res_id in num[0][0]
-        if res_id[1] != '-'] # ungap the sequence
+        if res_id[1] != '-']
         for num in numbering 
         if num
     ]
@@ -85,6 +95,9 @@ def number_sequences(sequences: Dict[str, SeqRecord], scheme: str = 'chothia') -
 def contains_single_model(structure: Structure) -> bool:
     return len(structure.get_list()) == 1
 
+def new_numbering_ends_on_higher_reseqid(old2new: Tuple) -> bool:
+    return old2new[-1][0][1] < old2new[-1][1][1]
+
 def renumber_structure(structure: Structure, numbering: Dict) -> None:
     """Takes a numbering dictionary and a structure and updates the residue IDs with the new numbering 
 
@@ -93,10 +106,18 @@ def renumber_structure(structure: Structure, numbering: Dict) -> None:
         numbering (Dict):
     """
     assert contains_single_model(structure), 'Structure contains more than one model'
+    
     for name, numbering in numbering.items():
-        res_ids = [res.id for res in structure[0][name].get_residues()][numbering.span]
+        res_ids = [res.id for res in structure[0][name].get_residues()]
+        fv_res_ids = res_ids[numbering.span]
+        non_fv_res_ids = set(res_ids) - set(fv_res_ids)
+        detach_children(structure[0][name], non_fv_res_ids) # kicks out residues that are not part of the Fv
+        old2new = list(zip(fv_res_ids, numbering.numbering))
+
+        if new_numbering_ends_on_higher_reseqid(old2new):
+            old2new = reversed(old2new) # prevents residue id assignment clashing with existing residue id 
         
-        for old, new in zip(res_ids, numbering.numbering):
+        for old, new in old2new: 
             structure[0][name][old].id = new
         
         structure[0][name].id = numbering.chain
@@ -128,15 +149,22 @@ def write_pdb(path: str, structure: Structure) -> None:
 
 def main(argv=None):
     parser = argparse.ArgumentParser(description='Renumber the Fv portion of a pdb file')
-    parser.add_argument('infile', type=str, help='Input file')
-    parser.add_argument('outfile', type=str, help='Output file')
+    parser.add_argument('infiles', nargs='+', type=str, help='Input file')
+    parser.add_argument('outdir', type=str, help='Output directory')
+    parser.add_argument('scheme', type=str, help='Numbering scheme')
     args = parser.parse_args(argv)
-    structure = get_structure(pathlib.Path(args.infile))
-    sequences = get_structure_sequences(structure)
-    numbering = number_sequences(sequences)
-    renumber_structure(structure, numbering)
-    write_pdb(args.outfile, structure)
+    outdir = pathlib.Path(args.outdir)
 
+    for pdb in args.infiles:
+        path = pathlib.Path(pdb)
+        structure = get_structure(path)
+        sequences = get_structure_sequences(structure)
+        numbering = number_sequences(sequences, scheme = args.scheme)
+        renumber_structure(structure, numbering)
+
+        outpath = (outdir/f'{path.stem}_{args.scheme}_Fv').with_suffix('.pdb')
+        write_pdb(str(outpath), structure)
+        
 if __name__ == '__main__':
     exit(main())
     
