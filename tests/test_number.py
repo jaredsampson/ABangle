5dfc99725d010ea4d6cf5c512a9a1da64016eedb
Jared Sampson <jared.sampson@gmail.com>
2026-07-07 17:01:37 -0400

    Get pytest running properly.

--- tests/test_number.py
+++ tests/test_number.py
@@ -4,10 +4,10 @@ from Bio.PDB.Structure import Structure
 
 @pytest.fixture
 def structure(shared_datadir):
-    return get_structure(shared_datadir, '1U8L.pdb')
+    return get_structure(shared_datadir / '1u8l.pdb')
 
 def test_get_structure(shared_datadir):
-    assert isinstance(get_structure(shared_datadir, '1U8L.pdb'), Structure)
+    assert isinstance(get_structure(shared_datadir / '1u8l.pdb'), Structure)
 
 def test_get_structure_sequence(structure):
     assert True 
